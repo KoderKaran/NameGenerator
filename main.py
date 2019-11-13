3a40c8ea0681799c8bfa5648a80b24fc99bd81c2
KoderKaran <karanrkhullar@gmail.com>
2019-11-12 22:25:42 -0500

    fixed treemaker

--- main.py
+++ main.py
@@ -11,32 +11,29 @@ class Request:
     def __init__(self, trait, start_yr, end_yr):
         self.trait = trait  # MAKE A TREE OF SYNONYMS FOR TRAIT AND CHECK EACH NAME AGAINST THIS.
         self.init_url = "https://www.thesaurus.com/browse/"
+        self.url = "https://www.thesaurus.com/browse/" + trait
         self.start_year = start_yr
         self.end_year = end_yr
         self.word_tree = [trait]
         self.count = 0
 
     def treeMaker(self, url):
-        for i in url:
-            page = requests.get(i)
-            data = page.text
-            soup = BeautifulSoup(data)
-            table = soup.find('ul', 'css-1lc0dpe et6tpn80')
-            related_text = [x.text for x in table.find_all('a')]
-            links_to_next = []
-            if self.count < 5:
-                try:
-                    for i in range(5):
-                        self.word_tree.append(related_text[i])
-                        links_to_next.append(self.init_url + related_text[i])
-                except IndexError:
-                    pass
-            else:
-                self.count = 0
+        page = requests.get(url)
+        data = page.text
+        soup = BeautifulSoup(data)
+        table = soup.find('ul', 'css-1lc0dpe et6tpn80')
+        related_text = [x.text for x in table.find_all('a')]
+        while True:
+            for i in range(5):
+                print(related_text[i])
+                self.word_tree.append(related_text[i])
             self.count += 1
-            self.treeMaker(links_to_next)
+            if self.count <= 10:
+                self.treeMaker(self.init_url + related_text[i])
+            else:
+                print("done")
+                break
+        self.word_tree = list(set(self.word_tree))
 
 
-
-test = Request("Confident", 1000, 1500)
-test.treeMaker()
+test = Request("ass", 1000, 1500)
