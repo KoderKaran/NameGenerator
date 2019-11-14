3335dfdbfc41cf453856f4302868c571f5032800
KoderKaran <karanrkhullar@gmail.com>
2019-11-14 01:35:17 -0500

    made searcher

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import os
 import re
+import json
+import time
+import random as ra
 
 # USE THESE SITES TO CHECK:
 # https://www.behindthename.com/name/john
@@ -14,7 +17,7 @@ class Request:
         self.gender = gender.upper()
         self.init_url = "https://www.thesaurus.com/browse/"
         self.url = "https://www.thesaurus.com/browse/" + trait
-        self.name_url1 = "https://www.names.org/" # NAME + /about
+        self.name_url1 = "https://www.definitions.net/definition/"
         self.name_url2 = "https://www.behindthename.com/name/" # NAME
         self.start_year = start_yr
         self.end_year = end_yr
@@ -24,6 +27,7 @@ class Request:
         self.names_to_check = []
         self.count = 0
         self.results = {}
+        self.times=0
 
     def tree_maker(self, url):
         page = requests.get(url)
@@ -36,19 +40,26 @@ class Request:
                 print(related_text[i])
                 self.word_tree.append(related_text[i])
             self.count += 1
-            if self.count <= 10:
+            if self.count <= 3:
                 self.tree_maker(self.init_url + related_text[i])
             else:
                 print("done")
                 break
         self.word_tree = list(set(self.word_tree))
 
-    def word_getter(self):
+    def file_getter(self):
         os.chdir(self.path)
         all_files = os.listdir(self.path)
-        count = 0
+        index1 = 0
+        index2 = 0
         for file in all_files:
-            with open(file, 'r') as f:
+            if str(self.start_year) in file:
+                index1 = all_files.index(file)
+            if str(self.end_year) in file:
+                index2 = all_files.index(file)
+        needed_files = all_files[index1:index2+1]
+        for new_file in needed_files:
+            with open(new_file, 'r') as f:
                 self.open_files.append(f.read())
 
     def name_getter(self):
@@ -62,19 +73,48 @@ class Request:
             gender = g[1]
             if gender == self.gender:
                 needed_names.append(g[0])
-        self.names_to_check = list(set(needed_names))
+        new_names = [ra.choice(needed_names) for x in range(1000)]
+        self.names_to_check = list(set(new_names))
 
     def name_searcher(self):
+        # second_link_catcher = re.compile("(?<=Meaning & History)(.*)(?=See All Relations)", re.MULTILINE | re.DOTALL)
+        acceptable = ["Princeton's", "Wiktionary", "Freebase", "Webster Dictionary", "Chambers 20th", "The Nuttall"]
+        print(len(self.names_to_check))
+        start = time.time()
         for name in self.names_to_check:
+            now = time.time()
+            print(name)
             words_in_meaning = []
-            url1 = self.name_url1 + name + "/about"
-            url2 = self.name_url2 + name
+            url1 = self.name_url1 + name
+            # url2 = self.name_url2 + name
             page1 = requests.get(url1)
-            page2 = requests.get(url2)
+            # page2 = requests.get(url2)
             data1 = page1.text
-            data2 = page2.text
+            # data2 = page2.text
             soup1 = BeautifulSoup(data1, features="html.parser")
-            soup2 = BeautifulSoup(data2, features="html.parser")
+            # soup2 = BeautifulSoup(data2, features="html.parser")
+            full_text = ''
+            for i in soup1.findAll('div', {'class':'rc5'}):
+                for j in acceptable:
+                    if j in i.text:
+                        full_text+=i.text.strip()
+            # for x in soup2.findAll('article'):
+            #     all_matches = re.findall(second_link_catcher, x.text)
+            #     for i in all_matches:
+            #         full_text+=i.strip()
+            text_list = full_text.split()
+            for word in text_list:
+                if word in self.word_tree:
+                    words_in_meaning.append(word)
+            if len(words_in_meaning) > 0:
+                self.results[name] = len(words_in_meaning)
+            print(time.time()-now)
+            self.times+=time.time()-now
+        print("avg time: " + str(self.times/ len(self.names_to_check)))
+        print("total: " + str(time.time()-start))
+        with open("test.json", "w+") as file:
+            json.dump(self.results, file, indent=4)
+
             # get text from both soup1 and 2
             # whenever word from self.word_tree in text, append that word to words in meaning
             # store len of words_in_meaning
@@ -84,9 +124,8 @@ class Request:
             # value in self.results
 
 
-
-
-test = Request("ass", "f", 1000, 1500)
+test = Request("Confident", "M", 1880, 1980)
 test.tree_maker(test.url)
-test.word_getter()
+test.file_getter()
 test.name_getter()
+test.name_searcher()
