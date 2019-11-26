267d0b8f46d8e54fcfebdf1bcea1ff294762d922
KoderKaran <karanrkhullar@gmail.com>
2019-11-25 23:27:05 -0500

    finished

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ import time
 
 
 class Request:
-    def __init__(self, trait, gender, start_yr, end_yr):
-        self.trait = trait
+    def __init__(self, trait, gender, start_yr, end_yr, flag):
+        self.flag = flag
+        self.trait = trait.upper()
         self.gender = gender.upper()
         self.init_url = "https://www.thesaurus.com/browse/"
         self.url = "https://www.thesaurus.com/browse/" + trait
@@ -22,7 +23,7 @@ class Request:
         self.name_url2 = "https://www.behindthename.com/name/" # NAME
         self.start_year = start_yr
         self.end_year = end_yr
-        self.word_tree = [trait]
+        self.word_tree = [self.trait]
         self.path = "C:\\Users\\yugio\\PycharmProjects\\NameGenerator\\names"
         self.open_files = []
         self.names_to_check = []
@@ -30,6 +31,7 @@ class Request:
         self.fullNumOfWords = 5000
         self.numOfWords = 0
         self.count = 0
+        self.inp = 0
 
     def tree_maker(self, url):
         page = requests.get(url)
@@ -50,6 +52,21 @@ class Request:
         self.word_tree = list(set(self.word_tree))
         print(self.word_tree)
 
+    def check(self):
+        if self.flag:
+            with open("results.json") as f:
+                data = json.load(f)
+            res = []
+            for key in data.keys():
+                if self.trait in data[key]:
+                    res.append(key)
+            for word in range(self.inp):
+                if len(res) > 0:
+                    element = ra.choice(res)
+                    self.results.append(element)
+                    res.remove(element)
+        self.name_searcher()
+
     def file_getter(self):
         os.chdir(self.path)
         all_files = os.listdir(self.path)
@@ -64,11 +81,6 @@ class Request:
         for new_file in needed_files:
             with open(new_file, 'r') as f:
                 self.open_files.append(f.read())
-
-    def time_getter(self):
-        inp = int(input("How many minutes do you have?"))
-        self.time = inp * 60
-        return int(10 * self.time)
 
     def name_getter(self):
         # make it loop through name amounts then plot it to see the times.
@@ -85,16 +97,17 @@ class Request:
         num = self.num_getter()
         new_names = [ra.choice(needed_names) for x in range(self.fullNumOfWords)]
         self.names_to_check = list(set(new_names))
-        self.name_searcher(num)
+        self.check()
 
     def num_getter(self):
         inp = int(input("How many names do you want to get?"))
+        self.inp = inp
         return inp
 
-    def name_searcher(self, how_many):
+    def name_searcher(self):
         now = time.time()
         for name in self.names_to_check:
-            if len(set(self.results)) >= how_many:
+            if len(set(self.results)) >= self.inp:
                 break
             print("\r", name, self.results, end='')
             url1 = self.name_url1 + name
@@ -104,7 +117,6 @@ class Request:
             count = 0
             for i in soup1.findAll('div', {'class':'rc5'}):
                 word_list = i.text.upper().strip().split()
-                print(word_list)
                 for word in self.word_tree:
                     if word in word_list:
                         count += 1
@@ -134,7 +146,15 @@ class Request:
 
 
 if __name__ == '__main__':
-    test = Request("Cocky", "M", 1980, 1990)
+    trait = input("What trait to search for?")
+    gender = input("What gender to search for?(M/F)")
+    startyr = int(input("Start year?"))
+    endyr = int(input("End year?"))
+    inp = input("Want results from database?Y/N")
+    flag = True
+    if inp == 'N':
+        flag = False
+    test = Request(trait, gender, startyr, endyr, flag)
     test.tree_maker(test.url)
     test.file_getter()
     test.name_getter()
